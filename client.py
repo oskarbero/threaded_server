d11061192730d11757311c4466e8693ad8d440d7
Oskar B <berooskar@gmail.com>
2017-03-23 18:10:47 -0400

    Initial commit

---
+++ client.py
@@ -0,0 +1,14 @@
+"""
+This is the client portion of the client server program
+"""
+
+
+def client():
+
+    # client code
+    return
+
+
+
+client()
+exit(0)
---
+++ server.py
@@ -0,0 +1,16 @@
+"""
+This is the server portion of a multithreaded server / client program
+"""
+
+
+def main():
+
+    # server code
+
+    return
+
+
+
+
+main()
+exit(0)
