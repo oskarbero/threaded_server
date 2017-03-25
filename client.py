18bbf1c7877c2e6243779ec8fa200520a8dbdf63
Caroline Cukier <caroc95@bu.edu>
2017-03-25 16:53:02 -0400

    My Bad-- Need to recommit working server
    Accidentally reverted; recommitting working server and generic client
    (for the heck of it)

--- client.py
+++ client.py
@@ -5,7 +5,10 @@ import socket
 
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('127.0.0.1', 47989))
-    data = 'some data'
-    sock.sendto(data.encode('utf-8'),('127.0.0.1', 47989))
+    sock.connect(('127.0.0.1', 42000))
+    data = 'some nice shiny data'
+    sock.sendto(data.encode('utf-8'), ('127.0.0.1', 42000))
+    print("client data sent")
+    sock.recv(1024)
+    print("client data received")
     sock.close()
