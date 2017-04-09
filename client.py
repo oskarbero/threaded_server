252d54d5f7db74777ddbbd12ae5a1b515e970fa0
Oskar B <berooskar@gmail.com>
2017-04-08 21:15:23 -0400

    Adding client loop for sending commands between the server and the client.

--- client.py
+++ client.py
@@ -3,12 +3,24 @@ This is the client portion of the client server program
 """
 import socket
 
+# CARO_IP = '50.163.98.9'
+MY_SOCKET = 42000
 if __name__ == "__main__":
+    ip_addr = input ("Ip: ")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('127.0.0.1', 42000))
-    data = 'some nice shiny data'
-    sock.sendto(data.encode('utf-8'), ('127.0.0.1', 42000))
-    print("client data sent")
-    sock.recv(1024)
+
+    sock.connect((ip_addr, MY_SOCKET))
+    cmd = ''
+    while cmd is not 'q':
+        cmd = input('Command: ')
+        sock.sendto(cmd.encode('utf-8'), ('192.168.10.108', 42000))
+
+        data = sock.recv(1024)
+        print(str(data.decode('utf-8')))
+
     print("client data received")
     sock.close()
+
+
+
+
